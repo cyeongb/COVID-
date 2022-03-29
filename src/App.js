770723dc1d34d1fd281c3e0e87fd4821f67b9e1f
cyeongb <cyeongb@gmail.com>
2022-03-30 00:22:36 +0900

    covid api data fetch 하기 작업

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
-
+import { fetchData } from "./api";
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log("fetchdata -> ", data);
+  }
+
   render() {
     return (
-      <div className="container">
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
