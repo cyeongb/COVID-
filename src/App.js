483232e70c19c78a1f959d04aaa0e0427467f0fd
cyeongb <cyeongb@gmail.com>
2022-03-30 22:02:14 +0900

    프로젝트 구조 짜기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { FormControl } from "@material-ui/core";
 import React from "react";
+import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return <div></div>;
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>covid19 tracker!!</h1>
+      <FormControl className="app__dropdown"></FormControl>
+
+      {/* Header */}
+      {/* Title + select input dropdown */}
+      {/* Info Box 1 */}
+      {/* Info Box 2 */}
+      {/* Info Box 3 */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
 }
 
 export default App;
